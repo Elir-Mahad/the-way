b4b54d709ed5fa391048c092680047df8049a5d6
elir-mahad <mmahad280@gmail.com>
2021-05-05 15:33:29 -0400

    Added the education category quotes in the eduscreen component. Converted the css into styled components for the quotes. Added a menu component to both the homepage and the education page. User can now go to education page, by click on education; and go to the homepage by clicking home.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import EduScreen from "./components/EducationScreen";
+import Menu from "./components/Menu";
 import WelcomeScreen from "./components/WelcomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <WelcomeScreen />
+      <Router>
+        <Switch>
+          <Route path="/education">
+            <Menu />
+            <EduScreen />
+          </Route>
+          {/*  */}
+          <Route path="/">
+            <Menu />
+            <WelcomeScreen />
+          </Route>
+          {/*  */}
+        </Switch>
+      </Router>
     </div>
   );
 }
